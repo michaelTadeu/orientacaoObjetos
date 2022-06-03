6272f29e838df80cec26964415f9f5ceb27b8d6f
michaelTadeu <michael89.oliveira@gmail.com>
2022-06-03 18:58:39 -0300

    Aula 1 de Orientação a Objetos.

---
+++ Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1OrientacaoAObjetos
+{
+    class Cliente
+    {
+        public string? nome;
+        public int codigo;
+    }
+}
